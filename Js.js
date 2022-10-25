215266582ec2758995a7e02a8231f0a3b1e020fc
Linux - Trent <trentblack@byui.edu>
2022-10-25 12:22:55 -0600

    more

--- Js.js
+++ Js.js
@@ -10,7 +10,7 @@ require([
 
 ], function (esriConfig, Map, MapView, FeatureLayer) {
 
-    esriConfig.apiKey = "AAPK207ed80aa65741f49bd0e50e559f22c3u3fBg8yzCSII7B2ML7cf4vyaDpvM9duMgIPK6n7MywF-qS27Lq95nash9vdx9dge";
+    esriConfig.apiKey = "AAPK4a952c6ab0284423ad07c4c434517838bUkrSQ2ipDmiQ0LSvX00IkpQViF_oViGknF53Yaw8Q5L26D8bIBP80JhXGxkle43";
 
     const map = new Map({
         basemap: "arcgis-topographic"
@@ -24,24 +24,13 @@ require([
     });
 
     
-    // Wind mills by england 
-    document.getElementById('Energy').onclick = function () {
-        const layer1 = new FeatureLayer({
-            url: "https://services3.arcgis.com/nGV4jiurzcahJ9LV/ArcGIS/rest/services/Offshore_Wind_Crown_Estate_Scotland/FeatureServer/0"
-        });
-        map.removeAll();
-
-        map.add(layer1, 1);
-        document.getElementById("right").innerText = "windmills are right out side of new england"
-
-            
-    }
+    
 
     document.getElementById('Nuclear').onclick = function () {
         const layer1 = new FeatureLayer({
             url: "https://services2.arcgis.com/FiaPA4ga0iQKduv3/ArcGIS/rest/services/NRC_Regions_1/FeatureServer/0"
         });
-        document.getElementById("right").innerText = "These are the different nuclear regions in america"
+        document.getElementById("right").innerText = "Description: These are the different nuclear regions in america"
         map.removeAll();
 
         map.add(layer1, 2);
@@ -50,17 +39,43 @@ require([
         const layer1 = new FeatureLayer({
             url: " https://services2.arcgis.com/FiaPA4ga0iQKduv3/ArcGIS/rest/services/eGRID_Subregions_1/FeatureServer/1"
         });
-        document.getElementById("right").innerText = "eGRID subregions ensure that power system demand and supply are finely balanced"
+        document.getElementById("right").innerText = "Description: eGRID subregions ensure that power system demand and supply are finely balanced"
         map.removeAll();
 
         map.add(layer1, 3);
     }
 
-   
+    document.getElementById('Fedreal').onclick = function () {
+        const layer1 = new FeatureLayer({
+            url: "https://services.arcgis.com/P3ePLMYs2RVChkJx/ArcGIS/rest/services/USA_National_Park_Service_Lands_20170930/FeatureServer/0"
+        });
+        document.getElementById("right").innerText = "Description: The US National Park Service manages 84.4 million acres that include the nation's 59 national parks, many national monuments, and other conservation and historical properties"
+        map.removeAll();
 
+        map.add(layer1, 3);
+    }
+    document.getElementById('EPA').onclick = function () {
+        const layer1 = new FeatureLayer({
+            url: "https://services.arcgis.com/cJ9YHowT8TU7DUyn/ArcGIS/rest/services/FRS_INTERESTS_AIR_MAJOR/FeatureServer/0"
+        });
+        document.getElementById("right").innerText = "Description: The Facility Registry Service (FRS) provides quality facility data to support EPA's mission of protecting human health and the environment by identifying and geospatially locating facilities, sites, or places subject to environmental regulations of environmental interest."
+        map.removeAll();
 
+        map.add(layer1, 3);
+    }
+    document.getElementById('Lines').onclick = function () {
+        const layer1 = new FeatureLayer({
+            url: "https://services2.arcgis.com/FiaPA4ga0iQKduv3/arcgis/rest/services/US_Electric_Power_Transmission_Lines/FeatureServer"
+        });
+        document.getElementById("right").innerText = "Description:  utilizing data from Homeland Infrastructure Foundation-Level Data (HIFLD), depicts electric power transmission lines in the United States"
+        map.removeAll();
+
+        map.add(layer1, 3);
+    }
+    
+    
     document.getElementById('RemoveALL').onclick = function () {
-       
+        document.getElementById("right").innerText = "Description: "
         map.removeAll();
 
 
